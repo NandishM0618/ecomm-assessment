aa6cad78a672b0fbda2a224af2a6400e7f4ca2bc
NandishM0618 <nandish1729@gmail.com>
2025-10-28 19:14:28 +0530

    feat(frontend): integrate backend API products with frontend and display list of products

--- src/frontend/app/components/Products.js
+++ src/frontend/app/components/Products.js
@@ -1,9 +1,76 @@
+'use client'
+import { useEffect, useState } from 'react';
+import apiClient from '../../services/apiService'
+import Link from 'next/link';
+
 export default function Products() {
+    const [products, setProducts] = useState([]);
+
+    const api = new apiClient();
+
+    async function getProducts(params) {
+        try {
+            const res = await api.get("products");
+            console.log(res.products);
+            setProducts(res.products);
+        } catch (error) {
+            console.log("falied to fetch products")
+        }
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, [])
+
     return (
         <>
-            <div className="pt-20 text-center">
-                <h1 className="text-4xl font-bold" id="products">Products List</h1>
-            </div>
+            <div className="pt-20 text-center mb-8">
+                <h1 className="text-4xl font-bold mb-8" id="products">Products List</h1>
+                <div className="max-w-6xl mx-auto grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-4">
+                    {products && products.length > 0 ? (
+                        products.map((product, index) => (
+                            <Link
+                                href="/"
+                                key={index}
+                                className="bg-white cursor-pointer rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300"
+                            >
+                                {product.images && product.images.length > 0 && (
+                                    <img
+                                        src={product.images[0].url}
+                                        alt={product.name}
+                                        className="w-full h-48 object-cover"
+                                    />
+                                )}
+                                <div className="p-5 flex flex-col justify-between h-full">
+                                    <div>
+                                        <h3 className="text-xl font-semibold mb-2 text-gray-900">{product.name}</h3>
+                                        <p className="text-sm text-gray-600 mb-4">
+                                            {product.description?.substring(0, 80)}...
+                                        </p>
+                                        <div className="flex items-center justify-between">
+                                            <span className="text-lg font-bold text-green-600">
+                                                <span className='text-black/70'>Price:</span> ₹{product.price}
+                                            </span>
+                                            <button
+                                                className="px-4 py-2 cursor-pointer bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-medium rounded-lg shadow-md hover:shadow-lg hover:from-blue-600 hover:to-indigo-700 active:scale-95 transition-all duration-200"
+                                                onClick={(e) => {
+                                                    e.preventDefault(); // prevent link navigation
+                                                    console.log(`Added ${product.name} to cart`);
+
+                                                }}
+                                            >
+                                                Add to Cart
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        ))
+                    ) : (
+                        <p className="text-gray-500 text-lg col-span-full">No products found</p>
+                    )}
+                </div >
+            </div >
         </>
     )
 }
---
+++ src/frontend/services/apiService.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+export default class apiClient {
+    constructor() {
+        this.apiUrl = 'http://localhost:8080/api';
+        this.client = axios.create({
+            baseURL: this.apiUrl,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        this.client.interceptors.response.use((config) => {
+            const token = localStorage.getItem('token');
+            if (token) {
+                config.headers["Authorization"] = `Bearer ${token}`;
+            }
+            return config
+        });
+    }
+
+    async post(endpoint, data) {
+        try {
+            const res = await this.client.post(`/${endpoint}`, data);
+        } catch (error) {
+            return { error: error.response?.data?.error || error.message };
+        }
+    }
+
+    async get(endpoint, data) {
+        try {
+            const res = await this.client.get(`/${endpoint}`);
+            return res.data
+        } catch (error) {
+            return { error: error.response?.data?.error || error.message };
+        }
+    }
+}
