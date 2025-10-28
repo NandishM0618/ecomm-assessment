7daaf62bbfa8b339b5bcbce98484a702774cf3f7
NandishM0618 <nandish1729@gmail.com>
2025-10-28 12:26:42 +0530

    project intialize with structure

---
+++ src/backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
